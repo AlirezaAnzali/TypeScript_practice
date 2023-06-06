16377cfd3cbb0d99fa5ebc61d268f97d61ec1f0c
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-06 11:39:42 -0400

    basics-06-arrays

--- app.ts
+++ app.ts
@@ -4,7 +4,34 @@
 // } = {
 const person = {
     name: "Alireza",
-    age: 35
+    age: 35,
+    hobbies: ["Sports", "Cooking"]
 }
 
+let favoriteActivities: string[]; 
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
+
+
+// const product:{
+//   id: string;
+//   price: number;
+//   tags: string[];
+//   details: {
+//     title: string;
+//     description: string;
+//   }
+// } = {
+//   id: "abc1",
+//   price: 12.99,
+//   tags: ["great-offer", "hot-and-new"],
+//   details: {
+//     title: "Red Carpet",
+//     description: "A great carpet - almost brand-new!",
+//   },
+// };
