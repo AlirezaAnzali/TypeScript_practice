247d2eebddcd71915597735b711d231b4a22d8ed
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-08 13:12:05 -0400

    config-02-excluding-including

--- app.ts
+++ app.ts
@@ -1,14 +1,2 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = "Ali";
-if (typeof userInput === "string") {
-    userName = userInput;
-};
-
-function generateError(message: string, code: number): never {
-    throw { message: message, errorCode: code};
-}
-
-generateError ("An Error Occurred", 500);
+const userName = "Alireza";
+console.log(userName);
