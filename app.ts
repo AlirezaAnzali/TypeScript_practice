e925ee70cd812e854af1ce470288294c11fe4b2a
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-12 10:24:16 -0400

    cl-interfaces-03-private-and-public

--- app.ts
+++ app.ts
@@ -1,21 +1,39 @@
 class Department {
-    name: string;
-    constructor(n:string) {
-       this.name = n;
-    }
+  name: string;
+  private employees: string[] = [];
 
-    describe(this: Department){
-        console.log("Department: " + this.name);
-    }
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    // maybe we want some validation here
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation(){
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Alireza");
+accounting.addEmployee("Max");
+
+// accounting.employees[2]= "Sara";  //Property 'employees' is private and only accessible within class 'Department'
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = {
-    name: "DUMMY",
-    describe: accounting.describe
-};
+// const accountingCopy = {
+//     name: "DUMMY",
+//     describe: accounting.describe
+// };
 
-accountingCopy.describe();
+// accountingCopy.describe();
