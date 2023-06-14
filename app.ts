5d9744d3fc652c85c1e13e75650f3faaf96198cd
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-14 13:37:22 -0400

    cl-interfaces-12-implementing-interfaces

--- app.ts
+++ app.ts
@@ -1,15 +1,43 @@
-interface Person {
-    name: string;
-    age: number;
-    greet(phrase: string):void;
+// interface Person {
+//     name: string;
+//     age: number;
+//     greet(phrase: string):void;
+// }
+// let user1 : Person;
+// user1 = {
+//     name: "Ali",
+//     age: 30,
+//     greet(phrase: string) {
+//         console.log(phrase + " " + this.name);
+//     }
+// }
+
+// user1.greet("Hi there, I am");
+
+
+interface Greetable {
+  name: string;
+  greet(phrase: string): void;
 }
-let user1 : Person;
-user1 = {
-    name: "Ali",
-    age: 30,
-    greet(phrase: string) {
+
+class Person implements Greetable {
+    name: string;
+
+    age = 30;
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    greet (phrase:string): void {
         console.log(phrase + " " + this.name);
     }
+
 }
+
+
+let user1: Greetable;
+
+user1 = new Person("Ali");
 
 user1.greet("Hi there, I am");
