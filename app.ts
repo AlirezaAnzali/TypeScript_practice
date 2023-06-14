ed9442e1a8267aa4792fce7fc593b65ac692044f
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-14 14:00:58 -0400

    cl-interfaces-14-interface-function-types

--- app.ts
+++ app.ts
@@ -14,6 +14,24 @@
 
 // user1.greet("Hi there, I am");
 
+
+
+
+
+// type AddFn = (a:number, b:number) => number;  // or we can use: 
+interface AddFn {
+  (a: number, b: number): number;
+}
+let add: AddFn;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+
+
+
+
+
 interface Named {
   readonly name: string;
 }
@@ -22,20 +40,18 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-    name: string;
+  name: string;
 
-    age = 30;
+  age = 30;
 
-    constructor(n: string) {
-        this.name = n;
-    }
+  constructor(n: string) {
+    this.name = n;
+  }
 
-    greet (phrase:string): void {
-        console.log(phrase + " " + this.name);
-    }
-
+  greet(phrase: string): void {
+    console.log(phrase + " " + this.name);
+  }
 }
-
 
 let user1: Greetable;
 
