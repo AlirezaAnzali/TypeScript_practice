ea2c4cd54c93c29e86022f636ace120051396377
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-16 11:58:25 -0400

    generics-04-keyof-constraints

--- app.ts
+++ app.ts
@@ -16,4 +16,37 @@ function merge<T extends object,U extends object> (objA: T, objB: U) {
 const mergedObj = merge({ name: "ALI" }, { age: 30 });
 console.log(mergedObj.age);
 
+//--------------------------------------------------------------------------------
 
+//another generic:
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy> (element: T): [T, string] {
+    let descriptionText = "Got no value!";
+    if (element.length === 1) {
+        descriptionText = "Got one element.";
+    } else if (element.length > 1) {
+      descriptionText =
+        "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hello World!"));
+console.log(countAndDescribe(["1", "2", "3"]));
+console.log(countAndDescribe(""));
+
+//-------------------------------------------------------------------------------
+
+//generics-keyof-constraints:
+
+function extractAndConvert<T extends object, U extends keyof T> (obj:T, key:U) {
+    return "value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Ali", age: 30 }, "name"));
+
+//-------------------------------------------------------------------------------
