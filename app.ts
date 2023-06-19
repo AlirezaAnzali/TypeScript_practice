d974283caba89e15b226768770bce5383049c7ec
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-19 16:50:07 -0400

    decorators-07-non-class-decorators-finished

--- app.ts
+++ app.ts
@@ -33,31 +33,60 @@ console.log(pers);
 
 //-----------------------------------------------------------------------------------
 
-function log(target:any, propertyName: string | symbol) {
-    console.log("Property Decorator!");
-    console.log(target, propertyName);
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
 }
 
-class product {
-    @log
-    title: string;
-    private _price: number;
-
-    set price(val: number) {
-        if (val>0) {
-            this._price = val;
-        } else {
-            throw new Error("Invalid price - should be positive!");
-        }
-    }
-
-    constructor(t: string, p:number) {
-        this.title = t;
-        this._price = p;
-    }
-
-    getPriceWithTax (tax:number) {
-        return this._price * (1+tax);
-    }
-
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
 }
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book 2", 29);
