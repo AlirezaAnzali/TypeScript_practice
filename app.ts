d0b5408ba1c2d349aaca8598bd6757ab1855d47c
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-19 14:11:35 -0400

    decorators-06-different-decorators

--- app.ts
+++ app.ts
@@ -29,3 +29,35 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+
+//-----------------------------------------------------------------------------------
+
+function log(target:any, propertyName: string | symbol) {
+    console.log("Property Decorator!");
+    console.log(target, propertyName);
+}
+
+class product {
+    @log
+    title: string;
+    private _price: number;
+
+    set price(val: number) {
+        if (val>0) {
+            this._price = val;
+        } else {
+            throw new Error("Invalid price - should be positive!");
+        }
+    }
+
+    constructor(t: string, p:number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax (tax:number) {
+        return this._price * (1+tax);
+    }
+
+}
