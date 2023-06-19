67e1775a4a15237161b081c6585a7da36b41805a
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-06-19 13:33:27 -0400

    decorators-03-adding-multiple-decorators

--- app.ts
+++ app.ts
@@ -5,13 +5,26 @@ function Logger (logString: string) {
     }
 }
 
-@Logger("LOGGING-PERSON")
-class Person {
-    name = "Ali";
-
-    constructor() {
-        console.log("Creating Person Object ...");
+function WithTemplate(template: string, hookId: string) {
+    return function(constructor:any) {
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1")!.textContent = p.name;
+        }
     }
+}
+
+@Logger("LOGGING-PERSON") // executed second
+@WithTemplate("<h1>My Person Object</h1>", "app") // executed first
+class Person {
+  name = "Ali";
+
+  constructor() {
+    console.log("Creating Person Object ...");
+  }
 }
 
 const pers = new Person();
